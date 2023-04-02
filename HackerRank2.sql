0c86bf014c00691c6ebd0be4dcda640548e022d6
GeorgeKovshov <GeorgGreat@gmail.com>
2023-04-02 21:07:00 +0300

    made a little query for top earners:

--- HackerRank2.sql
+++ HackerRank2.sql
@@ -13,4 +13,20 @@ SELECT ID, Name, REPLACE(CAST(Salary AS CHAR(15)), '0', '') AS Salar
 FROM EMPLOYEES;
 
 SELECT SUM(Salary) - SUM(CAST(REPLACE(CAST(Salary AS CHAR(15)), '0', '')as UNSIGNED))
-FROM EMPLOYEES
+FROM EMPLOYEES;
+
+SELECT t0.m, COUNT(e.salary * e.months)
+FROM (
+    SELECT MAX(months * salary) as m
+    FROM Employee
+) as t0
+JOIN Employee as e
+ON e.salary * e.months = t0.m
+GROUP by m;
+
+select (salary * months)as earnings ,count(*) 
+from employee 
+group by 1 
+order by earnings desc 
+limit 1;
+-- it meants group by first column from SELECT . The same pattern could be used for ORDER BY
